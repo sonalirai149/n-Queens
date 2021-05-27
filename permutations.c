c6ccbddcb7020391ba4c721d2d2723883407952f
sonalirai149 <66467235+sonalirai149@users.noreply.github.com>
2021-05-27 12:27:48 +0530

    Add files via upload

---
+++ permutations.c
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void find(int ind,vector<int> &nums,vector<vector<int>> &ans){
+        if(ind==nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=ind;i<nums.size();i++){
+            swap(nums[ind],nums[i]);
+            find(ind+1,nums,ans);
+            swap(nums[ind],nums[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        find(0,nums,ans);
+        return ans;
+    }
+};
